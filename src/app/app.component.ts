e42729eca295ca71e40d13d52197fa2181c6a649
Fernando Ferreira <fferreira12@gmail.com>
2022-05-11 21:28:51 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'BCREACT_MOD2_TP1';
+}
